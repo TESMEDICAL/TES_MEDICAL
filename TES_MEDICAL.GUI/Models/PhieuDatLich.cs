8227d68429b15d3fd59950f6fda6308f0645321d
ThanhDuyITC <duydt.itc@gmail.com>
2021-10-13 19:09:36 +0700

    FixValidate

--- TES_MEDICAL.GUI/Models/PhieuDatLich.cs
+++ TES_MEDICAL.GUI/Models/PhieuDatLich.cs
@@ -21,10 +21,10 @@ namespace TES_MEDICAL.GUI.Models
         [DataType(DataType.Date,ErrorMessage ="Ngày không hợp lệ")]
         [Required(ErrorMessage = "Bạn cần chọn ngày sinh")]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
-        public DateTime NgaySinh { get; set; }
+        public DateTime? NgaySinh { get; set; }
         [DataType(DataType.DateTime, ErrorMessage = "Ngày không hợp lệ")]
         [Required(ErrorMessage = "Bạn cần chọn ngày khám")]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
-        public DateTime NgayKham { get; set; }
+        public DateTime? NgayKham { get; set; }
     }
 }
