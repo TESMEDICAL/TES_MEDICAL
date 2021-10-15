fedfa0c1b677c0ca140e3353fcd5ea0757553582
DuongDTB-VN <duongdoan.0904@gmail.com>
2021-10-15 12:31:43 +0800

    Comit Done BacSi

---
+++ TES_MEDICAL.GUI/Controllers/BacSiController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TES_MEDICAL.GUI.Controllers
+{
+    public class BacSiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult PhieuKham()
+        {
+            return View();
+        }
+
+        public IActionResult KhamBenh()
+        {
+            return View();
+        }
+
+        public IActionResult LichSuKham()
+        {
+            return PartialView("_LichSuKham");
+        }
+
+        public IActionResult DanhSachThuoc()
+        {
+            return View();
+        }
+
+        public IActionResult ChiTietThuoc()
+        {
+            return PartialView("_ChiTietThuoc");
+        }
+    }
+}
