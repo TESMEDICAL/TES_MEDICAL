a5d693ce0d8731f8f921e9343f4ec33b128757f3
DuongDTB-VN <duongdoan.0904@gmail.com>
2021-10-16 12:56:27 +0800

    Comit Trua Admin

---
+++ TES_MEDICAL.GUI/Controllers/QuanLyTrangChuController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TES_MEDICAL.GUI.Controllers
+{
+    public class QuanLyTrangChuController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
