0c3e5a75aa7b38b396c189b76102a26509fd3272
Cao Tan Loc <loc.ct02@gmail.com>
2021-10-16 21:48:30 +0700

    Commit Edit Lich 16/10/2021

--- TES_MEDICAL.GUI/Controllers/TiepNhanController.cs
+++ TES_MEDICAL.GUI/Controllers/TiepNhanController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using TES_MEDICAL.GUI.Interfaces;
+using TES_MEDICAL.GUI.Models;
 
 namespace TES_MEDICAL.GUI.Controllers
 {
@@ -55,6 +56,39 @@ namespace TES_MEDICAL.GUI.Controllers
             return PartialView("_ChiTietDatLich", chiTietDatLich);
         }
 
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (await _service.GetPhieuDatLichById(id) == null)
+            {
+                return NotFound(); ;
+            }
+            else
+            {
+
+                return PartialView("_PartialViewEditLich", await _service.GetPhieuDatLichById(id));
+            }
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(PhieuDatLich model)
+        {
+
+            if (await _service.Edit(model) != null)
+                return Json(new { status = 1, title = "", text = "Cập nhật thành công.", obj = "" }, new Newtonsoft.Json.JsonSerializerSettings());
+            else
+                return Json(new { status = -2, title = "", text = "Cập nhật không thành công.", obj = "" }, new Newtonsoft.Json.JsonSerializerSettings());
+
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ReloadPage()
+        {
+            var listDatLich = await _service.GetAllPhieuDatLich();
+            return PartialView("_PartialReloadPage",listDatLich);
+        }
 
     }
 }
