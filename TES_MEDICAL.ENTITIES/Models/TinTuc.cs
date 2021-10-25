59435849985211576b63640e4ad9a57718481104
DinhNgocThangg <thang.dinhngoc1411@gmail.com>
2021-10-25 22:43:04 +0700

    validate_TinTuc_TheLoai_25/10

--- TES_MEDICAL.ENTITIES/Models/TinTuc.cs
+++ TES_MEDICAL.ENTITIES/Models/TinTuc.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
@@ -9,10 +10,16 @@ namespace TES_MEDICAL.GUI.Models
     {
         public Guid MaBaiViet { get; set; }
         public string Hinh { get; set; }
+
+        [Required(ErrorMessage = "Bạn cần nhập tiêu đề")]
         public string TieuDe { get; set; }
+
+        [Required(ErrorMessage = "Bạn cần nhập nội dung bài viết")]
         public string NoiDung { get; set; }
         public bool TrangThai { get; set; }
         public Guid MaNguoiViet { get; set; }
+
+        [Required(ErrorMessage = "Bạn cần chọn thể loại")]
         public Guid? MaTL { get; set; }
 
         public virtual NguoiDung MaNguoiVietNavigation { get; set; }
