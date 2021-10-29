97e405360419d51e804793f5987a8d8d9c73987a
DinhNgocThangg <thang.dinhngoc1411@gmail.com>
2021-10-29 15:16:25 +0700

    update_29/10

--- TES_MEDICAL.ENTITIES/Models/ViewModel/PhieuKhamViewModel.cs
+++ TES_MEDICAL.ENTITIES/Models/ViewModel/PhieuKhamViewModel.cs
@@ -8,13 +8,18 @@ namespace TES_MEDICAL.GUI.Models.ViewModel
 {
     public class PhieuKhamViewModel
     {
-        [Required(ErrorMessage ="Không để trống họ tên")]
+        [Required(ErrorMessage ="Bạn cần nhập họ tên")]
         public string HoTen { get; set; }
+        [Required(ErrorMessage = "Bạn cần nhập số điện thoại")]
+        [RegularExpression(@"^\(?([0-9]{3})[-. ]?([0-9]{4})[-. ]?([0-9]{3})$", ErrorMessage = "Số điện thoại không đúng")]
         public string SDT { get; set; }
         public string Email { get; set; }
+        [Required(ErrorMessage = "Bạn cần chọn ngày sinh")]
         public DateTime? NgaySinh { get; set; }
         public bool GioiTinh { get; set; }
+        [Required(ErrorMessage = "Bạn cần nhập địa chỉ")]
         public string DiaChi { get; set; }
+        [Required(ErrorMessage = "Bạn cần chọn bác sĩ")]
         public string MaBS { get; set; }
         public string TrieuChung { get; set; }
         public bool UuTien { get; set; }
