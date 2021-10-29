dec2b3c9939439422a1125a426e154ab34543544
DuongDTB-VN <duongdoan.0904@gmail.com>
2021-10-29 10:30:44 +0700

    Commt Report/ViewHoaDonThuoc 29/10

--- TES_MEDICAL.GUI/Controllers/DuocSiController.cs
+++ TES_MEDICAL.GUI/Controllers/DuocSiController.cs
@@ -5,20 +5,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using TES_MEDICAL.GUI.Interfaces;
-using TES_MEDICAL.GUI.Models;
 
 namespace TES_MEDICAL.GUI.Controllers
 {
     public class DuocSiController : Controller
     {
         private readonly IDuocSi _service;
-        private readonly IThuoc _thuocService;
-
-
-        public DuocSiController(IDuocSi service, IThuoc thuocService)
+        public DuocSiController(IDuocSi service)
         {
             _service = service;
-            _thuocService = thuocService;
         }
 
         [HttpGet]
@@ -27,58 +22,6 @@ namespace TES_MEDICAL.GUI.Controllers
             var listToaThuocCTT = await _service.GetAllToaThuocCTT(TrangThai);
             return Json(listToaThuocCTT);
             //return Ok(listToaThuocCTT);
-        }
-        
-
-        public async Task<IActionResult> ChangeSoUuTien(Guid maPK)
-        {
-            if (await _service.ChangeSoUuTien(maPK) != null)
-                return Json(new { status = 1, title = "", text = "Thay đổi thành công."}, new Newtonsoft.Json.JsonSerializerSettings());
-            else
-                return Json(new { status = -2, title = "", text = "Thay đổi thất bại."}, new Newtonsoft.Json.JsonSerializerSettings());
-        }
-
-        //Hàm thanh toán thuốc dùng trong Model ToaThuoc
-        [HttpPost]
-        public async Task<IActionResult> ThanhToan(Guid maPK)
-        {
-            var result = await _service.ThanhToanThuoc(maPK);
-            if (result != null)
-            {
-                return Json(new { status = 1, title = "", text = "Thanh toán thành công.", redirectUrL = Url.Action("ToaThuocDangPhat", "DuocSi"), obj = "" }, new JsonSerializerSettings());
-            }
-            else
-            {
-                return Json(new { status = -2, title = "", text = "Thanh toán không thành công", obj = "" }, new JsonSerializerSettings());
-            }
-        }
-
-
-        //Hàm xác nhận thuốc dùng trong Model ToaThuocDangPhat
-        [HttpPost]
-        public async Task<IActionResult> XacNhanThuocDangCho(Guid maPK)
-        {
-            var result = await _service.XacNhanThuocDangCho(maPK);
-            if (result != null)
-            {
-                return Json(new { status = 1, title = "", text = "Xác nhận thành công", redirectUrL = Url.Action("LichSuThuoc", "DuocSi"), obj = "" }, new JsonSerializerSettings());
-            }
-            else
-            {
-                return Json(new { status = -2, title = "", text = "Xác nhận không thành công", obj = "" }, new JsonSerializerSettings());
-            }
-        }
-
-
-        public IActionResult Index()
-        {
-            return View("ToaThuoc");
-        }
-
-        // ToaThuoc/Index
-        public IActionResult ToaThuoc()
-        {
-            return View();
         }
 
         public async Task<IActionResult> ChiTietToaThuoc(Guid MaPhieu)
@@ -90,85 +33,45 @@ namespace TES_MEDICAL.GUI.Controllers
         }
 
 
-        // Toathuoc/ToaThuocDangPhat
+        public IActionResult Index()
+        {
+            return View("ToaThuoc");
+        }
+
+        public IActionResult ToaThuoc()
+        {
+            return View();
+        }
+
+        
+
         public IActionResult ToaThuocDangPhat()
         {
             return View();
         }
 
-        public async Task<IActionResult> ChiTietDangPhat(Guid MaPhieu)
+        public IActionResult ChiTietDangPhat()
         {
-            var toaThuoc = await _service.GetToaThuocByMaPhieu(MaPhieu);
-            ViewBag.CTToaThuocDangPhat = await _service.GetChiTiet(MaPhieu);
-            return PartialView("_ChiTietDangPhat", toaThuoc);
+            return PartialView("_ChiTietDangPhat");
         }
-
-
 
         public IActionResult LichSuThuoc()
         {
             return View();
         }
-        public async Task<IActionResult> ChiTietLichSu(Guid MaPhieu)
+        public IActionResult ChiTietLichSu()
         {
-            var toaThuoc = await _service.GetToaThuocByMaPhieu(MaPhieu);
-            ViewBag.CTLichSuThuoc = await _service.GetChiTiet(MaPhieu);
-            return PartialView("_ChiTietLichSu", toaThuoc);
+            return PartialView("_ChiTietLichSu");
         }
 
-        public async Task<IActionResult> DanhSachThuoc(ThuocSearchModel model)
+        public IActionResult DanhSachThuoc()
         {
-            if (!model.Page.HasValue) model.Page = 1;
-            var listPaged = await _thuocService.SearchByCondition(model);
-
-            ViewBag.Names = listPaged;
-            ViewBag.Data = model;
-            return View(new ThuocSearchModel());
+            return View();
         }
 
-
-        [HttpGet]
-        public async Task<IActionResult> PageList(ThuocSearchModel model)
+        public IActionResult ChiTietThuoc()
         {
-
-            var listmodel = await _thuocService.SearchByCondition(model);
-            if (listmodel.Count() > 0)
-            {
-
-                if (!model.Page.HasValue) model.Page = 1;
-
-
-
-
-                ViewBag.Names = listmodel;
-                ViewBag.Data = model;
-
-                return PartialView("_NameListThuoc", listmodel);
-                //return View("DanhSachThuoc", listmodel);
-            }
-            else
-            {
-
-                return Json(new { status = -2, title = "", text = "Không tìm thấy", obj = "" }, new Newtonsoft.Json.JsonSerializerSettings());
-            }
-
-
-        }
-
-
-
-        public async Task<IActionResult> ChiTietThuoc(Guid id)
-        {
-            if (await _thuocService.Get(id) == null)
-            {
-                return NotFound(); ;
-            }
-            else
-            {
-
-
-                return PartialView("_ChiTietThuoc", await _thuocService.Get(id));
-            }
+            return PartialView("_ChiTietThuoc");
         }
 
     }
