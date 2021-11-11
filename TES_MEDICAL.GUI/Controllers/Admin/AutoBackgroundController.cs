19147e6253979b6462fe6d6c657f164d760fcf98
DuongDTB-VN <duongdoan.0904@gmail.com>
2021-11-11 11:50:10 +0800

    Comit LoginAdmin

--- TES_MEDICAL.GUI/Controllers/Admin/AutoBackgroundController.cs
+++ TES_MEDICAL.GUI/Controllers/Admin/AutoBackgroundController.cs
@@ -3,11 +3,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TES_MEDICAL.GUI.Controllers.Admin;
 using TES_MEDICAL.GUI.Interfaces;
 
 namespace TES_MEDICAL.GUI.Controllers
 {
-    public class AutoBackgroundController : Controller
+    public class AutoBackgroundController : BaseController
     {
         private readonly IAutoBackground _service;
         public AutoBackgroundController(IAutoBackground service)
