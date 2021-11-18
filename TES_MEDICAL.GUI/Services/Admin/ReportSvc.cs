8052e1880605700539357958e55508cc542e8437
DuongDTB-VN <duongdoan.0904@gmail.com>
2021-11-18 21:56:44 +0800

    Commit Add Layout

--- TES_MEDICAL.GUI/Services/Admin/ReportSvc.cs
+++ TES_MEDICAL.GUI/Services/Admin/ReportSvc.cs
@@ -36,12 +36,12 @@ namespace TES_MEDICAL.GUI.Services
 
         public async Task<IEnumerable<HoaDon>> GetAllHoaDon()
         {
-            return await _context.HoaDon.Include(x => x.MaNVNavigation).Include(x => x.MaPKNavigation).ThenInclude(x => x.MaBSNavigation).ToListAsync();
+            return await _context.HoaDon.Include(x => x.MaNVNavigation).Include(x => x.MaPKNavigation).ThenInclude(x => x.MaBSNavigation).OrderByDescending(x =>x.NgayHD).ToListAsync();
         }
 
         public async Task<IEnumerable<HoaDonThuoc>> GetAllHoaDonThuoc()
         {
-            return await _context.HoaDonThuoc.Include(x => x.MaNVNavigation).Include(x => x.MaPKNavigation).ThenInclude(x => x.MaPhieuKhamNavigation).ThenInclude(x => x.MaBSNavigation).ToListAsync();
+            return await _context.HoaDonThuoc.Include(x => x.MaNVNavigation).Include(x => x.MaPKNavigation).ThenInclude(x => x.MaPhieuKhamNavigation).ThenInclude(x => x.MaBSNavigation).OrderByDescending(x =>x.NgayHD).ToListAsync();
         }
 
         public async Task<HoaDonThuoc> GetTTHDThuoc(string MaHD)
