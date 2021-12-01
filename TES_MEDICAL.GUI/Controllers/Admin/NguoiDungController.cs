54f7c17b53e6446ab3efd2f72a4aba4896653e69
DuongDTB-VN <duongdoan.0904@gmail.com>
2021-12-01 21:58:02 +0800

    Commit AdminFeauture

--- TES_MEDICAL.GUI/Controllers/Admin/NguoiDungController.cs
+++ TES_MEDICAL.GUI/Controllers/Admin/NguoiDungController.cs
@@ -157,7 +157,7 @@ namespace TES_MEDICAL.GUI.Controllers
         [HttpPost]
         public async Task<ActionResult> Edit(NguoiDung model)
         {
-            var user = await _service.Get(Guid.Parse(HttpContext.Session.GetString(SessionKey.Nguoidung.MaNguoiDung)));
+            var user = await _service.Get(model.MaNguoiDung);
             user.ChucVu = model.ChucVu;
             user.TrangThai = model.TrangThai;
             var result = await _service.Edit(user);
